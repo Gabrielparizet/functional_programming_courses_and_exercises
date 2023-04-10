119c35284718106a2672d781c68e633ff9beda7f
Gabrielparizet <parizetgabriel@gmail.com>
2023-04-10 19:23:46 +0200

    Declarative vs Imperative programming

--- Courses/an_introduction_to_functional_programming.py
+++ Courses/an_introduction_to_functional_programming.py
@@ -1,3 +1,6 @@
+# This course is based on the article "An introduction to functional programming" by Mary Rose Cook
+# https://codewords.recurse.com/issues/one/an-introduction-to-functional-programming
+
 # UNFUNCTIONAL:
 a = 0
 def increment1():
@@ -48,3 +51,72 @@ from functools import reduce
 sum = reduce(lambda a, x: a + x, [0, 1, 2, 3, 4])
 print(sum)
 # => 10
+
+
+# This code counts how often the word ‘Sam’ appears in a list of strings:
+sentences = ['Mary read a story to Sam and Isla.',
+             'Isla cuddled Sam.',
+             'Sam chortled.']
+
+sam_count = 0
+for sentence in sentences:
+    sam_count += sentence.count('Sam')
+
+print(sam_count)
+# => 3
+
+
+# Same code written as a reduce:
+sam_count = reduce(lambda a, x: a + x.count('Sam'), sentences, 0)
+print(sam_count)
+
+
+
+# Write declaratively, not imperatively
+# from random import random
+
+# time = 5
+# car_positions = [1, 1, 1]
+
+# while time:
+#     # decrease time
+#     time -= 1
+
+    # print('')
+    # for i in range(len(car_positions)):
+    #     # move car
+    #     if random() > 0.3:
+    #         car_positions[i] += 1
+
+        # draw car
+        # print('-' * car_positions[i])
+
+
+# More declarative programming:
+from random import random
+
+def move_cars():
+    for i, _ in enumerate(car_positions):
+        if random() > 0.3:
+            car_positions[i] += 1
+
+def draw_car(car_position):
+    print('-' * car_position)
+
+def run_step_of_race():
+    global time
+    time -= 1
+    move_cars()
+
+def draw():
+    print('')
+    for car_position in car_positions:
+        draw_car(car_position)
+
+time = 5
+car_positions = [1, 1, 1]
+
+while time:
+    run_step_of_race()
+    draw()
+
--- Exercises/an_introduction_to_functional_programming.py
+++ Exercises/an_introduction_to_functional_programming.py
@@ -12,3 +12,40 @@ hashed_names = list(map(lambda x: hash(x), names))
 print(hashed_names)
 # Other solution 
 # hashed_names = map(hash, names)
+
+
+
+# Exercise 2. Try rewriting the code below using map, reduce and filter.
+people = [{'name': 'Mary', 'height': 160},
+    {'name': 'Isla', 'height': 80},
+    {'name': 'Sam'}]
+
+height_total = 0
+height_count = 0
+for person in people:
+    if 'height' in person:
+        height_total += person['height']
+        height_count += 1
+
+if height_count > 0:
+    average_height = height_total / height_count
+
+    print(average_height)
+    # => 120
+
+# Solution:
+from functools import reduce
+from operator import add
+people_with_heights = list(filter(lambda x: 'height' in x, people))
+print(people_with_heights)
+heights = list(map(lambda x: x['height'], people_with_heights))
+print(heights)
+average_height = reduce(add, heights) / len(heights)
+print('average height: ', average_height)
+
+# Correction:
+heights = map(lambda x: x['height'],
+              filter(lambda x: 'height' in x, people))
+
+if len(heights) > 0:
+    average_height = reduce(add, heights) / len(heights)
